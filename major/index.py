a4a0136dbdd1a0a1aa72db4d89178b4165aa36fd
Frank Tran <frank.imagination@gmail.com>
2025-05-19 21:03:19 +1000

    Learning Flask
    very basics of flask down

--- major/index.py
+++ major/index.py
@@ -1,1 +1,25 @@
-print("Hello World")
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def home():
+    return render_template('index.html', title = 'Home')
+
+
+
+
+
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', person=name, title='hi')
+
+
+# main driver function
+if __name__ == '__main__':
+
+    # run() method of Flask class runs the application 
+    # on the local development server.
+    app.run(debug=True)
