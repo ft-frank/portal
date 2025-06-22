e15dc47c55bada42fea04c57a8eb5d9da4ad66cc
Frank Tran <frank.imagination@gmail.com>
2025-06-23 00:10:17 +1000

    Finished up pdf viewer

--- major/index.py
+++ major/index.py
@@ -1,12 +1,10 @@
-from flask import Flask, render_template, redirect, url_for, flash, Response, abort, send_from_directory, send_file
+from flask import Flask, render_template, redirect, url_for, flash, abort, send_from_directory
 from functools import wraps
-from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, String
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm, CSRFProtect
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, SelectField, FileField
+from wtforms import StringField, PasswordField, SubmitField, SelectField
 from wtforms.validators import InputRequired, Length, ValidationError, Email
 from flask_bcrypt import Bcrypt
 from flask_admin import Admin
@@ -14,10 +12,6 @@ from flask_admin.contrib.sqla import ModelView
 from werkzeug.utils import secure_filename
 import os
 import mimetypes
-
-
-
-
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -28,16 +22,10 @@ db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 csrf = CSRFProtect(app)
 
-
-
-
-
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
 
-
-#RBAC
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
@@ -49,65 +37,34 @@ def role_required(*roles):
             if not current_user.is_authenticated:
                 return login_manager.unauthorized()
             if current_user.role not in roles:
-                abort(403)  # Forbidden
+                abort(403)
             return f(*args, **kwargs)
         return decorated_function
     return wrapper
-
-# Databases
-
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
-    role = db.Column(db.String(20), nullable=False)  # 'admin', 'tutor', 'student'
-    student_details = db.relationship('StudentDetail', backref='user', uselist=False)
-
-
-class Class(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    students = db.relationship('StudentDetail', backref='class_')
-    questions = db.relationship('QuestionBank', backref='class_')
-    homework = db.relationship('HomeworkSol', backref='class_')
-
-
-class StudentDetail(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    class_id = db.Column(db.Integer, db.ForeignKey('class.id'), nullable = True)
+    role = db.Column(db.String(20), nullable=False)
     first_name = db.Column(db.String(50))
     last_name = db.Column(db.String(50))
     school = db.Column(db.String(100))
     grade = db.Column(db.String(20))
-    class_id = db.Column(db.Integer, db.ForeignKey('class.id'))
-    invoices = db.relationship('Invoice', backref='student')
-    submissions = db.relationship('HomeworkSubmission', backref='student')
+    class_ = db.relationship('Class', backref='users')
 
-
-class Invoice(db.Model):
+class Class(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    student_id = db.Column(db.Integer, db.ForeignKey('student_detail.id'))
-    amount = db.Column(db.Float, nullable=False)
-    is_paid = db.Column(db.Boolean, default=False)
-
+    name = db.Column(db.String(50), nullable=False)
 
 class HomeworkSol(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     filename = db.Column(db.String(100), nullable=False)
     class_id = db.Column(db.Integer, db.ForeignKey('class.id'))
-    file_path = db.Column(db.String(200))  # Path to locally stored file
-    submissions = db.relationship('HomeworkSubmission', backref='homework')
-
-
-
-class HomeworkSubmission(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    student_id = db.Column(db.Integer, db.ForeignKey('student_detail.id'))
-    homework_id = db.Column(db.Integer, db.ForeignKey('homework_sol.id'))
-    file_path = db.Column(db.String(200))  # path to uploaded file
-    submitted_on = db.Column(db.DateTime)
+    file_path = db.Column(db.String(200))
+    class_ = db.relationship('Class', backref='homework_solutions')
 
 
 class QuestionBank(db.Model):
@@ -115,229 +72,161 @@ class QuestionBank(db.Model):
     title = db.Column(db.String(100), nullable=False)
     filename = db.Column(db.String(100), nullable=False)
     class_id = db.Column(db.Integer, db.ForeignKey('class.id'))
-    file_path = db.Column(db.String(200))  # Path to locally stored file
+    file_path = db.Column(db.String(200))
+    class_ = db.relationship('Class', backref='question_banks')
 
-
-
-# Forms
 class RegisterForm(FlaskForm):
-    email = StringField(
-        validators=[
-            InputRequired(),
-            Email(message='Invalid email address'),
-            Length(min=5, max=50)
-        ],
-        render_kw={"placeholder": "Email", "type": "email"}
-    )
-
-    password = PasswordField(
-        validators=[InputRequired(), Length(min=4, max=20)],
-        render_kw={"placeholder": "Password"}
-    )
-
+    email = StringField(validators=[InputRequired(), Email(), Length(min=5, max=50)], render_kw={"placeholder": "Email", "type": "email"})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password"})
+    class_id = SelectField("Class", coerce=int)
+    first_name = StringField(validators=[InputRequired(), Length(min=0, max=50)])
+    last_name = StringField(validators=[Length(min=0, max=50)])
+    school = StringField(validators=[Length(min=0, max=50)])
+    grade = SelectField(
+        choices=[('12'), ('11'), ('None')],
+        validators=[InputRequired()],
+        render_kw={"placeholder": "Grade"})
     role = SelectField(
         choices=[('student', 'Student'), ('tutor', 'Tutor'), ('admin', 'Admin')],
         validators=[InputRequired()],
         render_kw={"placeholder": "Role"}
     )
-
     submit = SubmitField("Register")
 
     def validate_email(self, email):
-        existing_user = User.query.filter_by(email=email.data).first()
-        if existing_user:
-            raise ValidationError("That email is already in use. Please use a different one.")
+        if User.query.filter_by(email=email.data).first():
+            raise ValidationError("That email is already in use.")
 
 class LoginForm(FlaskForm):
-    email = StringField(
-        validators=[InputRequired(), Length(min=5, max=50)],
-        render_kw={"placeholder": "Email"}
-    )
-
-    password = PasswordField(
-        validators=[InputRequired(), Length(min=4, max=20)],
-        render_kw={"placeholder": "Password"}
-    )
-
+    email = StringField(validators=[InputRequired(), Length(min=5, max=50)])
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)])
     submit = SubmitField("Login")
 
 class HomeworkForm(FlaskForm):
     title = StringField('Title', validators=[InputRequired()])
-    file = FileField('Upload File', validators=[FileAllowed(['pdf', 'docx', 'png', 'jpg'], 'Only documents and images allowed'), InputRequired()])
+    class_id = SelectField("Class", coerce=int)
+    file = FileField('Upload File', validators=[FileAllowed(['pdf', 'docx', 'png', 'jpg']), InputRequired()])
     class_id = SelectField("Class", coerce=int, validators=[InputRequired()])
     submit = SubmitField('Create')
-
-
-
-# Pages
-
 
 @app.route('/')
 @login_required
 def home():
-    return render_template('home.html', title = 'Home', current_page = 'home', user = current_user)
-
-@app.route('/login', methods = ['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email = form.email.data).first()
-        if user:
-            if bcrypt.check_password_hash(user.password, form.password.data):
-                login_user(user)
-                return redirect(url_for('home'))
-    return render_template('login.html', title = 'Login', current_page = 'login', form = form)
-
-@app.route('/logout', methods = ['GET', 'POST'])
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('login'))
-
-@app.route('/register', methods = ['GET', 'POST'])
-def register():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data)
-        new_user = User(email=form.email.data, password = hashed_password, role = form.role.data)
-        db.session.add(new_user)
-        db.session.commit() 
-        flash("New Account created succesfully!", "success")
-        flash(f"Their email: {form.email.data}, Their password: {form.password.data}, Their role: {form.role.data}")
-
-    else:
-        flash("An existing account exists with this email.", "danger")
-    return render_template('register.html', title = 'Register', current_page = 'register', form = form)
+    return render_template('home.html', title='Home', current_page = 'home', user=current_user)
 
 @app.route('/invoices')
 @login_required
 def invoices():
     return render_template('invoices.html', title = 'Invoices', current_page = 'invoices')
 
-@app.route('/homework')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user)
+            if user.role == 'admin':
+                return redirect(url_for('admin.index'))  
+            elif user.role == 'student':
+                return redirect(url_for('home'))  
+            else:
+                return redirect(url_for('home')) 
+        flash("Incorrect email or password.", "danger")
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
 @login_required
-def homework():
-    homeworks = HomeworkSol.query.all()  # 👈 Get all homework
-    return render_template('homework.html', title='Homework', current_page='homework', homeworks=homeworks)
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    form.class_id.choices = [(c.id, c.name) for c in Class.query.all()]
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        user = User(email=form.email.data, password=hashed_password, class_id = form.class_id.data, first_name = form.first_name.data, last_name = form.first_name.data, school = form.school.data, grade = form.grade.data, role=form.role.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Account created successfully!", "success")
+    else:
+        flash("An existing account exists with this email.", "danger")     
+    return render_template('register.html', form=form)
+
+@app.route('/hw_solutions')
+@login_required
+def hw_solutions():
+    homeworks = HomeworkSol.query.filter_by(class_id = current_user.class_id)
+    return render_template('homework.html', homeworks=homeworks, user = current_user, current_page = 'hw_solutions')
+
+@app.route('/hw_solutions/<filename>')
+@login_required
+def show_homework_sol(filename):
+    if not filename.endswith('.pdf'):
+        abort(403)
+    folder = os.path.join(app.root_path, app.config['HOMEWORK_SOL_FOLDER'])
+    return send_from_directory(folder, filename)
+
+@app.route('/upload_homework_sol', methods=['GET', 'POST'])
+@role_required('admin', 'tutor')
+def create_homework():
+    form = HomeworkForm()
+    form.class_id.choices = [(c.id, c.name) for c in Class.query.all()]
+    if form.validate_on_submit():
+        filename = secure_filename(form.file.data.filename.replace(" ", "_").replace("%", "_"))
+        path = os.path.join(app.root_path, app.config['HOMEWORK_SOL_FOLDER'], filename)
+        form.file.data.save(path)
+        db.session.add(HomeworkSol(title=form.title.data, filename=filename, class_id=form.class_id.data, file_path=path))
+        db.session.commit()
+        flash("Homework uploaded successfully!", "success")
+    return render_template('create_homework.html', form=form)
 
 @app.route('/questions')
 @login_required
 def questions():
-    return render_template('questions.html', title='Questions', current_page = 'questions')
+    questions = QuestionBank.query.filter_by(class_id = current_user.class_id)
+    return render_template('questions.html', questions=questions, current_page = 'questions', user = current_user)
 
-@app.route('/settings')
+@app.route('/questions/<filename>')
 @login_required
-def settings():
-    return render_template('settings.html', title='Settings', current_page = 'settings')
+def show_questions(filename):
+    if not filename.endswith('.pdf'):
+        abort(403)
+    folder = os.path.join(app.root_path, app.config['QUESTIONS_FOLDER'])
+    return send_from_directory(folder, filename)
 
 @app.route('/upload_questions', methods=['GET', 'POST'])
 @role_required('admin', 'tutor')
 def upload_questions():
     form = HomeworkForm()
     form.class_id.choices = [(c.id, c.name) for c in Class.query.all()]
-
     if form.validate_on_submit():
-        
-        file = form.file.data
-        path = os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['QUESTIONS_FOLDER'], secure_filename(file.filename))
-        file.save(path)
-        questions = QuestionBank(
-            title = form.title.data,
-            filename = file.filename,
-            class_id = form.class_id.data,
-            file_path = path)
-        db.session.add(questions)
+        filename = secure_filename(form.file.data.filename.replace(" ", "_").replace("%", "_"))
+        path = os.path.join(app.root_path, app.config['QUESTIONS_FOLDER'], filename)
+        form.file.data.save(path)
+        db.session.add(QuestionBank(title=form.title.data, filename=filename, class_id=form.class_id.data, file_path=path))
         db.session.commit()
-        flash("Question uploaded to question bannk succesfully!", "success")
-        return render_template('create_questions.html', form = form, title = 'Create Questions')
+        flash("Question uploaded successfully!", "success")
+    return render_template('create_questions.html', form=form)
 
-
-    return render_template('create_questions.html', form=form, title='Create Questions')
-
-@app.route('/questions/<filename>/')
-@login_required
-def show_questions(filename):
-    if not filename.endswith('.pdf'):
-        abort(403)  # Only allow PDFs
-
-    folder = os.path.join(app.root_path, 'static', 'questions')
-    return send_from_directory(folder, filename, as_attachment=False, mimetype=mimetypes.guess_type(filename)[0])
-
-@app.route('/hw_solutions')
-@login_required
-def hw_solutions():
-    homeworks = HomeworkSol.query.all()  # 👈 Get all homework
-    return render_template('solutions.html', title='HomeworkSolutions', current_page = 'hw_solutions', homeworks = homeworks)
-
-@app.route('/upload_homework_sol', methods=['GET', 'POST'])
-@login_required
-@role_required('admin', 'tutor')
-def create_homework():
-    form = HomeworkForm()
-    form.class_id.choices = [(c.id, c.name) for c in Class.query.all()]
-    if form.validate_on_submit():
-        
-        file = form.file.data
-        path = os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['HOMEWORK_SOL_FOLDER'], secure_filename(file.filename))
-        file.save(path) #Saves File to Database
-        homework = HomeworkSol(
-            title = form.title.data,
-            filename = file.filename,
-            class_id = form.class_id.data,
-            file_path = path)
-        db.session.add(homework)
-        db.session.commit() 
-        flash("Homework Solutions uploaded succesfully!", "success")
-    return render_template('create_homework.html', form = form)
-
-@app.route('/hw_solutions/<filename>/')
-@login_required
-def show_homework_sol(filename):
-    if not filename.endswith('.pdf'):
-        abort(403)  # Only allow PDFs
-
-    folder = os.path.join(app.root_path, 'static', 'homework_sol')
-    return send_from_directory(folder, filename, as_attachment=False, mimetype=mimetypes.guess_type(filename)[0])
-
-
-
-
-
-#Admin View
+# Admin setup
 class AdminModelView(ModelView):
-    def is_accessible(self):
-        return current_user.is_authenticated and current_user.role == 'admin'
+    None
+    # def is_accessible(self):
+    #     return current_user.is_authenticated and current_user.role == 'admin'
 
-    def inaccessible_callback(self, name, **kwargs):
-        return redirect(url_for('login'))
-    
-class HomeworkModelView(AdminModelView):
-    form_columns = ['title', 'filename', 'class_id', 'file_path']
-    form_args = {
-        'class_id': {
-            'label': 'Class',
-            'query_factory': lambda: Class.query.all(),
-            'allow_blank': False
-        }
-    }
+    # def inaccessible_callback(self, name, **kwargs):
+    #     return redirect(url_for('login'))
 
-admin = Admin(app, name='Admin Panel', template_mode='bootstrap4')
+admin = Admin(app, name='Admin Panel')
 admin.add_view(AdminModelView(User, db.session))
-admin.add_view(AdminModelView(StudentDetail, db.session))
-admin.add_view(AdminModelView(Invoice, db.session))
 admin.add_view(AdminModelView(Class, db.session))
 admin.add_view(AdminModelView(HomeworkSol, db.session))
-admin.add_view(AdminModelView(HomeworkSubmission, db.session))
 admin.add_view(AdminModelView(QuestionBank, db.session))
 
-
-
-
-# main driver function
 if __name__ == '__main__':
-
-    # run() method of Flask class runs the application 
-    # on the local development server
     with app.app_context():
-        db.create_all()  # 🔥 This creates the tables based on your models
+        db.create_all()
     app.run(debug=True)
